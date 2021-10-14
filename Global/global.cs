be26fb4c1e1826dfe368c6afdf7f254fc44f4425
SynI20N <leonid@dc.local>
2021-10-14 12:17:57 +0300

    added global

---
+++ Global/global.cs
@@ -0,0 +1,18 @@
+﻿using EleWise.ELMA.ConfigurationModel;
+using EleWise.ELMA.CRM.Models;
+using EleWise.ELMA.API;
+using EleWise.ELMA.Model.Managers;
+using EleWise.ELMA.Model.Services;
+using EleWise.ELMA.Model.Types.Settings;
+using System.Linq;
+
+namespace Prolog.Web.Global
+{
+    public class global
+    {
+        private void test(ISaleConfigExt entity)
+        {
+            
+        }
+    }
+}
