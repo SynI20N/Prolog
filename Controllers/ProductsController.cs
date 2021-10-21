67c52147399fb093cffff5bf0eb626e7dcbae39a
SynI20N <leonid@dc.local>
2021-10-21 16:29:34 +0300

    trying to fix filters bug 4

--- Controllers/ProductsController.cs
+++ Controllers/ProductsController.cs
@@ -42,7 +42,7 @@ namespace Prolog.Web.Controllers
                 {
                     if (statusFilter[i] == '1')
                     {
-                        ProductStatus status = (ProductStatus)0;
+                        ProductStatus status = ProductStatus.New;
                         ((TovarFilter)filter.Filter).StatusNew.Add(status);
                     }
                 }
